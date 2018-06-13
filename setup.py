047abd1c799cbfad98df121e074572eb3498e1af
Nikita Gillmann <nikita@n0.is>
2018-06-13 06:13:24 +0000

    setup

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 setup(
     name = 'manga-scrapper',
     packages = ['manga-scrapper'],
-    version = '0.1',
+    version = "0.1",
     description = 'Manga-scrapper is a small image scrapper based on BeautifulSoup4 and cfscrape',
-    author = ['Daniel Montivero', 'Nils Gillmann'],
-    author_email = ['danielelnuevo@hotmail.com', 'ng0@n0.is'],
-    url = 'https://c.n0.is/gillmann/manga-scrapper',
-    keywords = ['manga', 'scraping'],
+    author = "Daniel Montivero, Nils Gillmann (ng0)",
+    author_email = "danielelnuevo@hotmail.com, ng0@n0.is",
+    url = "https://c.n0.is/gillmann/manga-scrapper",
+    keywords = "manga scraping",
+    classifiers = ['Development Status :: 3 - Alpha'],
+    platforms = ['Linux'],
     include_package_data = True,
     install_requires = ['cfscrape', 'BeautifulSoup']
 )
