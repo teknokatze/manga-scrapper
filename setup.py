ef86d512ff5a3de41ea5d6521270c34dbe97f788
Nikita Gillmann <nikita@n0.is>
2019-12-19 00:42:55 +0000

    change name

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ setup(
     packages = ['manga-scrapper'],
     version = "0.1",
     description = 'Manga-scrapper is a small image scrapper based on BeautifulSoup4 and cfscrape',
-    author = "Daniel Montivero, Nils Gillmann (ng0)",
+    author = "Daniel Montivero, ng0",
     author_email = "danielelnuevo@hotmail.com, ng0@n0.is",
-    url = "https://c.n0.is/gillmann/manga-scrapper",
+    url = "https://c.n0.is/manga-scrapper",
     keywords = "manga scraping",
     classifiers = ['Development Status :: 3 - Alpha'],
-    platforms = ['Linux'],
     entry_points = {
         "console_scripts": [
             "manga_scrapper = manga_scrapper.__main__:main",
